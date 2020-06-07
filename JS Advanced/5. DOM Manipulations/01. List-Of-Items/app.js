096a3dd5f12c5dc4e2acc980acca98be640c7d72
ivanv3lkov <Ivanv3lkov@gmail.com>
2020-06-07 13:14:37 +0300

    'Added'

--- JS Advanced/5. DOM Manipulations/01. List-Of-Items/app.js
+++ JS Advanced/5. DOM Manipulations/01. List-Of-Items/app.js
@@ -2,6 +2,11 @@ function addItem() {
     let text = document.getElementById('newItemText').value;
     let itemsList = document.getElementById('items');
     let li = document.createElement('li');
+    
+    if (text.length === 0) {
+        return;
+    };
+
     li.textContent = text;
     itemsList.appendChild(li)
 
