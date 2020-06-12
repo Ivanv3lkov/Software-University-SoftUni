f83bc65d9c4541c404bd015102b27adedc1d6a0c
ivanv3lkov <Ivanv3lkov@gmail.com>
2020-06-12 21:30:03 +0300

    'Added'

---
+++ JS Advanced/6. Function context/08. HEX.js
@@ -0,0 +1,33 @@
+class Hex {
+    constructor(value) {
+        this.value = value;
+    }
+
+    valueOf() {
+        return this.value;
+    }
+
+    toString() {
+        return '0x' + this.value.toString(16).toUpperCase();
+    }
+
+    plus(hex) {
+       return new Hex(this.value + hex)
+    }
+
+    minus(hex) {
+        return new Hex(this.value - hex)
+    }
+
+    static parse(hexValue) {
+        return parseInt(hexValue, 16);
+    }
+}
+
+let FF = new Hex(255);
+console.log(FF.toString());
+FF.valueOf() + 1 == 256;
+let a = new Hex(10);
+let b = new Hex(5);
+console.log(a.plus(b).toString());
+console.log(a.plus(b).toString()==='0xF');
