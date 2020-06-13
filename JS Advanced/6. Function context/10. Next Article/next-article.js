9babd4339689713adc6aedc0a3ed21c97cd002c6
ivanv3lkov <Ivanv3lkov@gmail.com>
2020-06-13 09:25:39 +0300

    'Added'

---
+++ JS Advanced/6. Function context/10. Next Article/next-article.js
@@ -0,0 +1,13 @@
+function getArticleGenerator(articles) {
+    const content = document.querySelector('#content');
+
+    function showNext() {
+        if (articles.length > 0) {
+            let article = document.createElement('article');
+            article.textContent = articles.shift();
+            content.appendChild(article);
+        }
+    }
+
+    return showNext;
+}
