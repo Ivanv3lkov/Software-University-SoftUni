07fef2668b0e3cfd1f45460923fb0dd44670107b
ivanv3lkov <Ivanv3lkov@gmail.com>
2020-06-17 22:47:27 +0300

    'Added'

---
+++ JS Advanced/8. Object Composition/01. Order Rectangles.js
@@ -0,0 +1,26 @@
+function solution(input) {
+    function createRect(width, height) {
+        let rect = {
+            width: width,
+            height: height,
+            area: function () {
+                return rect.width * rect.height;
+            },
+            compareTo: function (other) {
+                return other.area() - rect.area() || other.width - rect.width;
+            }
+        };
+
+        return rect;
+    }
+
+    let rects = [];
+    for (let [width, height] of input) {
+        rects.push(createRect(width, height));
+    }
+
+    let sortedRects = rects.sort((a, b) => a.compareTo(b));
+    return sortedRects;
+}
+
+console.log(solution([[10, 5], [5, 12]]));
