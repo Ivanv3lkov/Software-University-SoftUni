bd3e857609a20e389156a33b6ffb78c630c915cb
ivanv3lkov <Ivanv3lkov@gmail.com>
2020-06-19 23:45:46 +0300

    'Added'

---
+++ JS Advanced/8. Object Composition/07. Construction Crew.js
@@ -0,0 +1,18 @@
+function solve(worker) {
+    if (worker.dizziness) {
+        worker.levelOfHydrated += 0.1 * worker.weight * worker.experience,
+        worker.dizziness = false
+    }
+    
+    return worker;
+}
+
+
+console.log(solve({
+    weight: 120,
+    experience: 20,
+    levelOfHydrated: 200,
+    dizziness: true
+}));
+
+
