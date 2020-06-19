dbd96f2341bf54b715b30b518a7b615f8a8db5af
ivanv3lkov <Ivanv3lkov@gmail.com>
2020-06-19 23:46:34 +0300

    'Added'

---
+++ JS Advanced/8. Object Composition/09. Extensible Object.js
@@ -0,0 +1,28 @@
+function solve() {
+    const proto = {};
+    const instance = Object.create(proto);
+
+    instance.extend = function (template) {
+        for (let prop in template) {
+            if (typeof template[prop] === 'function') {
+                proto[prop] = template[prop];
+            } else {
+                instance[prop] = template[prop]
+            }
+        };
+    }
+    return instance;
+}
+
+const myInstance = solve();
+myInstance.extend({
+    extensionMethod: function () {/*do something */},
+    extensionProperty: 'someString'
+});
+
+console.log('intance props: ', myInstance);
+console.log('prototype proprs ', Object.getPrototypeOf(myInstance));
+
+
+
+
