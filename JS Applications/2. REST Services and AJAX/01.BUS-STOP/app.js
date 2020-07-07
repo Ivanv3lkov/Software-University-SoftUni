c3fd372724ecab7a2f740339be497c57ea8e9523
ivanv3lkov <Ivanv3lkov@gmail.com>
2020-07-07 20:40:42 +0300

    'Added'

--- JS Applications/2. REST Services and AJAX/01.BUS-STOP/app.js
+++ JS Applications/2. REST Services and AJAX/01.BUS-STOP/app.js
@@ -1,3 +1,28 @@
 function getInfo() {
-    console.log("TODO...");
+    const stopIdInput = document.getElementById('stopId');
+    const stopNameDiv = document.getElementById('stopName');
+    const busContainer = document.getElementById('buses')
+
+    const busesUrl = `https://judgetests.firebaseio.com/businfo/${stopIdInput.value}.json `;
+
+    stopIdInput.value = '';
+    stopNameDiv.textContent = '';
+    busContainer.innerHTML = '';
+    
+    fetch(busesUrl)
+        .then(res => res.json())
+        .then(data => {
+            const { name, buses } = data;
+            stopNameDiv.textContent = name;
+            Object.entries(buses)
+            .forEach(([busId, busTime]) => {
+                const li = document.createElement('li');
+                li.textContent = `Bus ${busId} arrives in ${busTime} minutes.`;
+
+                busContainer.appendChild(li);
+            })
+        })
+        .catch((err) => {
+            stopNameDiv.textContent = 'Error'
+        })
 }
