bcc67525821b4589583b714b39db2ba862f45474
ivanv3lkov <Ivanv3lkov@gmail.com>
2020-07-07 21:19:57 +0300

    'Added'

--- JS Applications/2. REST Services and AJAX/04.MESSENGER/app.js
+++ JS Applications/2. REST Services and AJAX/04.MESSENGER/app.js
@@ -1,5 +1,42 @@
 function attachEvents() {
-    console.log('TODO...');
-}
+    let sendBtn = document.getElementById("submit");
+    let refreshBtn = document.getElementById("refresh");
+    let authorElement = document.getElementById("author");
+    let contentElement = document.getElementById("content");
+    let chatHistory = document.getElementById("messages");
 
-attachEvents();
+    refreshBtn.addEventListener("click", function(){
+        fetch("https://rest-messanger.firebaseio.com/messanger.json")
+        .then(res=>res.json())
+        .then(data=>{
+            Object.entries(data)
+            .forEach(([key,msgData])=>{
+                let {author, content} = msgData;
+                let textToAdd = `${author}: ${content}\n`;
+                chatHistory.value += textToAdd;
+            });
+        })
+        .catch(err=>{
+            console.log(err);
+        });
+    });
+
+    sendBtn.addEventListener("click",function(){
+        let author = authorElement.value;
+        let content = contentElement.value;
+
+        if(author && content) {
+            let headers = {
+                method : "POST",
+                headers : {"Content-Type" : "application/json"},
+                body : JSON.stringify({author, content})
+            };
+    
+            fetch("https://rest-messanger.firebaseio.com/messanger.json",headers)
+            .then(()=>{
+                authorElement.value = "";
+                contentElement.value = "";
+            });
+        }
+    });
+}
