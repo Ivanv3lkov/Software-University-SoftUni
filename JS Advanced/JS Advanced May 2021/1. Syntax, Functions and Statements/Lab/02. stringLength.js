a9dd61c64c29fdc314c91b7d425472dd44331a67
ivanv3lkov <ivanv3lkov@gmail.com>
2021-05-24 12:24:03 +0300

    Add 02. stringLength.js

---
+++ JS Advanced/JS Advanced May 2021/1. Syntax, Functions and Statements/Lab/02. stringLength.js
@@ -0,0 +1,8 @@
+function stringLenght(...args) {
+    let sumLengthStr = args.map(str => str.length).reduce((acc, curVal) => acc + curVal, 0)
+    let avgLength = Math.floor(sumLengthStr / args.length)
+    console.log(sumLengthStr); 
+    console.log(avgLength);
+}
+
+stringLenght('chocolate', 'ice cream', 'cake')
