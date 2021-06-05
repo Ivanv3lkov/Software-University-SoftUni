12499fd9e206584ac0328be68710917654af67cd
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-05 19:48:32 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/4. DOM/Lab/04.Show More/ShowMore.js
+++ JS Advanced/JS Advanced May 2021/4. DOM/Lab/04.Show More/ShowMore.js
@@ -1,3 +1,6 @@
 function showText() {
-    // TODO
+   let text = document.querySelector('#text');
+   let a = document.querySelector('#more');
+   text.style.display = 'inline';
+   a.style.display = 'none';
 }
