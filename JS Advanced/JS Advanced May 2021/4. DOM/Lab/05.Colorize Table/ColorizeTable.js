e3cae1dbf08674bdf5d8fc19cb60e998edb7bf60
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-05 20:04:21 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/4. DOM/Lab/05.Colorize Table/ColorizeTable.js
+++ JS Advanced/JS Advanced May 2021/4. DOM/Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,4 @@
 function colorize() {
-    // TODO
+    let tableRows = Array.from(document.querySelectorAll('table tr'));
+    tableRows.map((row, index) => index % 2 === 1 ? row.style.backgroundColor = 'Teal' : '')
 }
