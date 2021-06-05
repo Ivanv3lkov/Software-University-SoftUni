42ccd7f5e7aa170f0081ffc63c1f695d1291b1d6
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-05 19:32:41 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/4. DOM/Lab/01.Edit Element/EditElement.js
+++ JS Advanced/JS Advanced May 2021/4. DOM/Lab/01.Edit Element/EditElement.js
@@ -1,3 +1,7 @@
-function editElement() {
-    // TODO
+function editElement(element, match, replacer) {
+    let newText = element.textContent;
+    while (newText.includes(match)) {
+        newText = newText.replace(match, replacer)
+    }
+    element.textContent = newText;
 }
