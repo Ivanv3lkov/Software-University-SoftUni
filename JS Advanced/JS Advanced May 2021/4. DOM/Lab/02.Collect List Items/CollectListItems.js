67e901a3fd36471065dc7851d2b20876f4a01ceb
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-05 19:33:20 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/4. DOM/Lab/02.Collect List Items/CollectListItems.js
+++ JS Advanced/JS Advanced May 2021/4. DOM/Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,9 @@
 function extractText() {
-    // TODO
+    const listElements = Array.from(document.querySelector('#items').children);
+    const textArea = document.querySelector('#result');
+    let output = '';
+    for (let li of listElements) {
+        output += li.textContent + '\n';
+    }
+    textArea.textContent = output.trim();
 }
--- JS Advanced/JS Advanced May 2021/4. DOM/Lab/03.Sum Numbers/SumNumbers.js
+++ JS Advanced/JS Advanced May 2021/4. DOM/Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,6 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    let firstNum = Number(document.querySelector('#num1').value);
+    let secondNum = Number(document.querySelector('#num2').value);
+    let sum =  document.querySelector('#sum');
+    sum.value = firstNum + secondNum;
 }
