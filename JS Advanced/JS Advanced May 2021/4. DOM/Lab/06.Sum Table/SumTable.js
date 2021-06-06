6e0b08607903b08543bd681d32a8a51255036057
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-06 10:24:51 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/4. DOM/Lab/06.Sum Table/SumTable.js
+++ JS Advanced/JS Advanced May 2021/4. DOM/Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,6 @@
 function sumTable() {
-
+    let costs = Array.from(document.querySelectorAll('td:nth-child(2)'));
+    let sum = costs.pop();
+    let totalSum = costs.reduce((acc, currValue) => acc + Number(currValue.textContent), 0);
+    sum.textContent = totalSum;
 }
