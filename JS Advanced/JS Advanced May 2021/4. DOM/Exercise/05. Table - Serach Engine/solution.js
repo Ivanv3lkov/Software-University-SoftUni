0c2ecd4f2335a86906f8af9f9012af55b8800d41
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-07 23:46:09 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/4. DOM/Exercise/05. Table - Serach Engine/solution.js
+++ JS Advanced/JS Advanced May 2021/4. DOM/Exercise/05. Table - Serach Engine/solution.js
@@ -2,7 +2,17 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const inputElement = document.getElementById('searchField');
+      const rows = Array.from(document.getElementsByTagName('tr')).slice(2);
+      rows.forEach(row => {
+         row.classList = '';
+      });
 
+      for (let row of rows) {
+         if (row.textContent.includes(inputElement.value)) {
+            row.classList = 'select';
+         }
+      }
+      inputElement.value = '';
    }
 }
