5d4914261d38face431a675c97cff1bebebf110a
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-07 23:03:50 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/4. DOM/Exercise/03. Accordion/accordion.js
+++ JS Advanced/JS Advanced May 2021/4. DOM/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,7 @@
 function toggle() {
-    console.log('TODO:...');
+   let button = document.querySelectorAll('.button')[0];
+   let text = document.querySelector('#extra');
+
+   button.textContent == 'More' ?  button.textContent = 'Less' : button.textContent = 'More';
+   button.textContent == 'More' ?  text.style.display = 'none' : text.style.display = 'block';  
 }
