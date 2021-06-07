9e2b92d711d3d97c924c388b8f272fcf86a158ab
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-07 23:22:55 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/4. DOM/Exercise/04. Search in List/search.js
+++ JS Advanced/JS Advanced May 2021/4. DOM/Exercise/04. Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+   const result = document.getElementById('result');
+   const input = document.getElementById('searchText');
+   const towns = Array.from(document.getElementById('towns').children);
+   let matches = 0;
+   towns.forEach(town => {
+      town.style.fontWeight = '';
+      town.style.textDecoration = 'none';
+   });
+
+   for (let town of towns) {
+      if (town.textContent.includes(input.value)) {
+         matches++;
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+      }
+   }
+   result.textContent = `${matches} matches found`;
 }
