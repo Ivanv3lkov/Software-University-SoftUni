6fc85795b12b2e037e4208bf14a051ad8823b5e6
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-07 21:36:33 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/4. DOM/Lab/03.Sum Numbers/SumNumbers.js
+++ JS Advanced/JS Advanced May 2021/4. DOM/Lab/03.Sum Numbers/SumNumbers.js
@@ -1,6 +1,6 @@
 function calc() {
     let firstNum = Number(document.querySelector('#num1').value);
     let secondNum = Number(document.querySelector('#num2').value);
-    let sum =  document.querySelector('#sum');
+    let sum = document.querySelector('#sum');
     sum.value = firstNum + secondNum;
 }
--- JS Advanced/JS Advanced May 2021/4. DOM/Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS Advanced/JS Advanced May 2021/4. DOM/Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,5 @@
 function extract(content) {
-
+    let target = document.getElementById(content);
+    let text = target.textContent;
+    
 }
