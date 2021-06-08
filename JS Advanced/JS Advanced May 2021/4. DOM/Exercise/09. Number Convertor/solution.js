6c80a65679f9085984f1a96acc8f2524b0ade9a5
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-08 20:45:21 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/4. DOM/Exercise/09. Number Convertor/solution.js
+++ JS Advanced/JS Advanced May 2021/4. DOM/Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,44 @@
 function solve() {
+    const convertButton = document.querySelector("#container > button");
+    const selectMenuTo = document.getElementById('selectMenuTo');
 
-    //TODO...
+    convertButton.addEventListener('click', convert);
+
+    function convert() {
+        let number = Number(document.getElementById('input').value);
+        let result;
+        if (selectMenuTo.value === 'binary') {
+            result = decimalToBinary(number)
+        } else if (selectMenuTo.value === 'hexadecimal') {
+            result = hexadecimalToBinary(number)
+        }
+
+        appendResult(result);
+    }
+
+    function decimalToBinary(number) {
+        return (number >> 0).toString(2);
+    }
+
+    function hexadecimalToBinary(number) {
+        return number.toString(16).toUpperCase();
+    }
+
+    function appendResult(result) {
+        document.getElementById('result').value = result;
+    }
+
+    function createSelectMenuOptions() {
+        let binaryOption = document.createElement('option');
+        binaryOption.textContent = 'Binary';
+        binaryOption.value = 'binary';
+        let hexadecimalOption = document.createElement('option');
+        hexadecimalOption.textContent = 'Hexadecimal';
+        hexadecimalOption.value = 'hexadecimal';
+
+        selectMenuTo.appendChild(binaryOption);
+        selectMenuTo.appendChild(hexadecimalOption);
+    }
+
+    createSelectMenuOptions();
 }
