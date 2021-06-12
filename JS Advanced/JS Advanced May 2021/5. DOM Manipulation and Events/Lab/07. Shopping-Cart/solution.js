676c24d6f04a44dc5230cba53915f367b286b2e9
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-12 18:18:44 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/5. DOM Manipulation and Events/Lab/07. Shopping-Cart/solution.js
+++ JS Advanced/JS Advanced May 2021/5. DOM Manipulation and Events/Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-   //TODO...
+   let addProductButtons = Array.from(document.querySelectorAll('.add-product'));
+   let textarea = document.querySelector('textarea');
+   let checkoutButton = document.querySelector('.checkout');
+   let productSet = new Set();
+   let totalPrice = 0;
+
+   addProductButtons.forEach(button => button.addEventListener('click', addButtonHandler));
+   checkoutButton.addEventListener('click', checkoutHandler);
+
+   function addButtonHandler(e) {
+      let productPrice = Number(e.target.parentElement.nextElementSibling.textContent);
+      let productName = e.target.parentElement.parentElement.querySelector('.product-title').textContent;
+      totalPrice += productPrice;
+      textarea.value += `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`
+      productSet.add(productName);
+   }
+
+   function checkoutHandler() {
+      productSet = Array.from(productSet);
+      textarea.value += `You bought ${productSet.join(', ')} for ${totalPrice.toFixed(2)}.`;
+      disableButtons();
+   }
+
+   function disableButtons() {
+      let buttons = Array.from(document.querySelectorAll('button'));
+      buttons.forEach(button => button.disabled = true);
+   }
 }
