a7292a65b8dd2515abe2341ccff1758d5d660092
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-13 20:36:02 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/5. DOM Manipulation and Events/Exercise/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/JS Advanced May 2021/5. DOM Manipulation and Events/Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    const [newItemTextElement, newItemValueElement] = document.getElementsByTagName('input');
+    if (!newItemTextElement.value || !newItemValueElement.value) {return}
+    let menu = document.getElementById('menu');
+    let option = document.createElement('option');
+    option.textContent = newItemTextElement.value;
+    option.value = newItemValueElement.value;
+    menu.appendChild(option);
+    
+    newItemTextElement.value = '';
+    newItemValueElement.value = '';
 }
--- JS Advanced/JS Advanced May 2021/5. DOM Manipulation and Events/Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS Advanced/JS Advanced May 2021/5. DOM Manipulation and Events/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,26 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const [textAreaSender, textAreaReceiver] = document.getElementsByTagName('textarea');
+    const [encondeButton, decodeButton] = document.getElementsByTagName('button');
+
+    encondeButton.addEventListener('click', encode);
+    decodeButton.addEventListener('click', decode);
+
+    function encode() {
+        const message = textAreaSender.value;
+        let encodedMessage = '';
+        for (let i = 0; i < message.length; i++) {
+            encodedMessage += String.fromCharCode(message.charCodeAt(i) + 1);
+        }
+        textAreaReceiver.value = encodedMessage;
+        textAreaSender.value = '';
+    }
+
+    function decode() {
+        const encodedMessage = textAreaReceiver.value;
+        let decodedMessage = '';
+        for (let i = 0; i < encodedMessage.length; i++) {
+            decodedMessage += String.fromCharCode(encodedMessage.charCodeAt(i) - 1);
+        }
+        textAreaReceiver.value = decodedMessage;
+    }
 }
