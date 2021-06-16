acca61755f7976d5dc3966203a0ab9e3af03ab07
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-16 21:58:36 +0300

    add problem solution

---
+++ JS Advanced/JS Advanced May 2021/6. Advanced functions/Lab/05. commandProcessor.js
@@ -0,0 +1,27 @@
+function solution() {
+    let result = '';
+    return {
+        append(str) {
+            result += str;
+        },
+        removeStart(n) {
+            result = result.substring(n);
+        },
+        removeEnd(n) {
+            result = result.substring(0, result.length - n);
+        },
+        print() {
+            console.log(result);
+        }
+    }
+}
+
+
+
+let firstZeroTest = solution();
+
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
