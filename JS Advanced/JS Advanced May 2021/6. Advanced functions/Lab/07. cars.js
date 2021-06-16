e7d136537540c94573e55ce2e4a5410cd8178fc2
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-16 22:18:48 +0300

    add problem solution

---
+++ JS Advanced/JS Advanced May 2021/6. Advanced functions/Lab/07. cars.js
@@ -0,0 +1,44 @@
+function cars(commands) {
+    let carCommandInterpreter = (function () {
+        let allCars = new Map;
+
+        function create(newObjName, token, protoName) {
+            if (token == null) {
+                allCars.set(newObjName, {});
+                return;
+            }
+
+            allCars.set(newObjName, Object.create(allCars.get(protoName)));
+        }
+
+        function set(objName, propName, propVal) {
+            allCars.get(objName)[propName] = propVal;
+        }
+
+        function print(objName) {
+            let res = [];
+            let obj = allCars.get(objName);
+            for (let prop in obj) {
+                res.push(`${prop}:${obj[prop]}`);
+            }
+
+            console.log(res.join(', '));
+        }
+
+        return { create, set, print };
+    })();
+
+    for (let command of commands) {
+        let tokens = command.split(/\s+/);
+        let commandName = tokens.shift();
+        carCommandInterpreter[commandName](...tokens);
+    }
+}
+
+cars(['create c1',
+    'create c2 inherit c1',
+    'set c1 color red',
+    'set c2 model new',
+    'print c1',
+    'print c2']
+);
