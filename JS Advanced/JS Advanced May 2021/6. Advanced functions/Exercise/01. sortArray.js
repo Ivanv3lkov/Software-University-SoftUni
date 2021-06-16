403155127dc8cb934a1ffa76bf8c9c929c382d0e
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-16 22:24:31 +0300

    add problem solution

---
+++ JS Advanced/JS Advanced May 2021/6. Advanced functions/Exercise/01. sortArray.js
@@ -0,0 +1,7 @@
+function sortArray(arr, criteria) {
+    return criteria == 'asc'
+        ? arr.sort((a, b) => a - b)
+        : arr.sort((a, b) => b - a);
+}
+
+console.log(sortArray([14, 7, 17, 6, 8], 'desc'));
