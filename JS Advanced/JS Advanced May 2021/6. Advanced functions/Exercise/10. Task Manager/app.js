47c791bf00deecdbcf89a07d7cc7d23f91f70d52
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-18 00:10:06 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/6. Advanced functions/Exercise/10. Task Manager/app.js
+++ JS Advanced/JS Advanced May 2021/6. Advanced functions/Exercise/10. Task Manager/app.js
@@ -1,3 +1,66 @@
 function solve() {
-    console.log("//TODO")
+    let taskInput = document.getElementById('task');
+    let textAreaInput = document.getElementById('description');
+    let dataInput = document.getElementById('date');
+    let addButton = document.getElementById('add');
+    let sections = Array.from(document.querySelectorAll('section'));
+    let openDiv = Array.from(sections[1].querySelectorAll('div'))[1]
+    let inProgressDiv = Array.from(sections[2].querySelectorAll('div'))[1]
+    let completedDiv = Array.from(sections[3].querySelectorAll('div'))[1]
+
+    addButton.addEventListener('click', addTask);
+
+    function addTask(e) {
+        e.preventDefault();
+        if (taskInput.value.trim() === '' || textAreaInput.value.trim() === '' || dataInput.value.trim() === '') { return; }
+
+        let article = createElement('article');
+        let h3 = createElement('h3', taskInput.value);
+        let p1 = createElement('p', `Description: ${textAreaInput.value}`);
+        let p2 = createElement('p', `Due Date: ${dataInput.value}`);
+        article.appendChild(h3);
+        article.appendChild(p1);
+        article.appendChild(p2);
+
+        let div = createElement('div');
+        div.setAttribute('class', 'flex');
+        let startButton = createElement('button', 'Start');
+        startButton.setAttribute('class', 'green');
+        let deleteButton = createElement('button', 'Delete');
+        deleteButton.setAttribute('class', 'red');
+        div.appendChild(startButton);
+        div.appendChild(deleteButton);
+        article.appendChild(div);
+
+        openDiv.appendChild(article);
+
+        startButton.addEventListener('click', (e) => {
+            let currentDiv = e.target.parentElement;
+            e.target.textContent === 'Start' ? e.target.remove() : '';
+            let finshButton = createElement('button', 'Finish');
+            finshButton.setAttribute('class', 'orange');
+            currentDiv.appendChild(finshButton);
+            inProgressDiv.appendChild(currentDiv.parentElement);
+
+            finshButton.addEventListener('click', (e) => {
+                let currentTask = e.target.parentElement.parentElement;
+                e.target.parentElement.remove();
+                completedDiv.appendChild(currentTask);
+            })
+        })
+
+        deleteButton.addEventListener('click', (e) => {
+            e.target.parentElement.parentElement.remove();
+        })
+    }
+
+    function createElement(type, text) {
+        let element = document.createElement(type);
+
+        if (text) {
+            element.textContent = text;
+        }
+
+        return element;
+    }
 }
