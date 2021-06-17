8ae0420f333ed0cd9f84cdfcb6efdabedee1e1fc
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-17 21:59:55 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/6. Advanced functions/Exercise/08. Next Article/script.js
+++ JS Advanced/JS Advanced May 2021/6. Advanced functions/Exercise/08. Next Article/script.js
@@ -1,3 +1,12 @@
 function getArticleGenerator(articles) {
-    // TODO
+    function showNext() {
+        if (articles.length !== 0) {
+            const articleToShow = articles.shift();
+            const articleElement = document.createElement('article');
+            articleElement.textContent = articleToShow;
+            document.querySelector('#content').appendChild(articleElement);
+        }
+    }
+
+    return showNext;
 }
