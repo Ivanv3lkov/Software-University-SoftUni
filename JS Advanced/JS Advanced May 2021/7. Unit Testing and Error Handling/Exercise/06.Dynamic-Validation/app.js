02be65468f959774b448a37e85323e09264c73d3
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-20 17:19:28 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/7. Unit Testing and Error Handling/Exercise/06.Dynamic-Validation/app.js
+++ JS Advanced/JS Advanced May 2021/7. Unit Testing and Error Handling/Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    // TODO
+    let emailInput = document.getElementById('email');
+    emailInput.addEventListener('change', validateEmail);
+    
+    function validateEmail() {
+        let emailRegex = /^[a-z]+@[a-z]+\.[a-z]+$/;
+        let isValid = emailRegex.test(emailInput.value);
+        if (isValid) {
+            emailInput.classList.remove('error');
+        } else {
+            emailInput.classList.add('error');
+        }
+    }
 }
