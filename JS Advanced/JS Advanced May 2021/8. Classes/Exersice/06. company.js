301dd3947578cb8bd6ddb57dd1e9ac68348da83a
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-24 22:32:53 +0300

    add problem solution

---
+++ JS Advanced/JS Advanced May 2021/8. Classes/Exersice/06. company.js
@@ -0,0 +1,49 @@
+class Company {
+    constructor() {
+        this.departments = new Map();
+    }
+
+    addEmployee(username, salary, position, department) {
+        if (!username || !salary || salary < 0 || !position || !department) {
+            throw new Error('Invalid input!');
+        }
+
+        let newEmployee = { username, salary, position, department };
+
+        if (!this.departments.has(department)) {
+            this.departments.set(department, [newEmployee]);
+        } else {
+            this.departments.get(department).push(newEmployee);
+        }
+
+        return `New employee is hired. Name: ${username}. Position: ${position}`;
+    }
+
+    bestDepartment() {
+        let totalSalary = (department) => {
+            let totalSalary = department[1].reduce((acc, b) => { return acc += b.salary }, 0);
+            let averageSalary = (totalSalary / department[1].length).toFixed(2);
+            department.push(averageSalary);
+            return averageSalary;
+        };
+        let bestDepartment = [...this.departments].sort((a, b) => totalSalary(b) - totalSalary(a))[0];
+        let sortBySalaryAndName = bestDepartment[1].sort((a, b) => b.salary - a.salary || a.username.localeCompare(b.username));
+
+        let result = `Best Department is: ${bestDepartment.shift()}\n`;
+        result += `Average salary: ${bestDepartment.pop()}\n`;
+        sortBySalaryAndName.forEach(e => result += `${e.username} ${e.salary} ${e.position}\n`);
+
+        return result.trim();
+    }
+}
+
+let c = new Company();
+c.addEmployee("Stanimir", 2000, "engineer", "Construction");
+c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
+c.addEmployee("Slavi", 500, "dyer", "Construction");
+c.addEmployee("Stan", 2000, "architect", "Construction");
+c.addEmployee("Stanimir", 1200, "digital marketing manager", "Marketing");
+c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
+c.addEmployee("Gosho", 1350, "HR", "Human resources");
+console.log(c.bestDepartment());
+
