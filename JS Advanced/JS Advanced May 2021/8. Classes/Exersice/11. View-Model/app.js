cf1c637e014c7b1d4ad041cc201f0cd23769af2f
ivanv3lkov <ivanv3lkov@gmail.com>
2021-06-24 23:03:40 +0300

    add problem solution

--- JS Advanced/JS Advanced May 2021/8. Classes/Exersice/11. View-Model/app.js
+++ JS Advanced/JS Advanced May 2021/8. Classes/Exersice/11. View-Model/app.js
@@ -1,5 +1,40 @@
 class Textbox {
-    //TODO
+    constructor(selector, invalidSymbolsRegex) {
+        this._elements = document.querySelectorAll(selector);
+        this._invalidSymbols = invalidSymbolsRegex;
+        this.onInput();
+    }
+
+    onInput() {
+        Array.from(this._elements).forEach(el => {
+            el.addEventListener('change', (e) => { 
+                this.value = e.target.value; 
+            });
+        });
+    }
+
+    get value() {
+        return this._elements[0].value;
+    }
+
+    set value(val) {
+        this._value = val;
+        for (const element of this.elements) {
+            element.value = val;
+        }
+    }
+
+    get elements() {
+        return this._elements;
+    }
+
+    isValid() {
+        if (this._invalidSymbols.test(this.elements[0].value)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
 
 let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
