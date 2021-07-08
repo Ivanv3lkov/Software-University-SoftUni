9eb1f81860b54ad7aa4ee2d96cdb6b67a50591fa
ivanv3lkov <ivanv3lkov@gmail.com>
2021-07-08 20:17:34 +0300

    Asynchronous programming exercise

--- JS Applications/JS Applications June 2021/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
+++ JS Applications/JS Applications June 2021/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
@@ -1,3 +1,26 @@
 function getInfo() {
-    console.log("TODO...");
+    const stopIdInput = document.getElementById('stopId');
+    const stopNameDiv = document.getElementById('stopName');
+    const busContainer = document.getElementById('buses');
+    const busesUrl = `http://localhost:3030/jsonstore/bus/businfo/${stopIdInput.value}`;
+
+    stopIdInput.value = '';
+    stopNameDiv.textContent = '';
+    busContainer.innerHTML = '';
+
+    fetch(busesUrl)
+        .then(res => res.json())
+        .then(data => {
+            const { name, buses } = data;
+            stopNameDiv.textContent = name;
+            Object.entries(buses)
+                .forEach(([busId, busTime]) => {
+                    const li = document.createElement('li');
+                    li.textContent = `Bus ${busId} arrives in ${busTime} minutes.`;
+                    busContainer.appendChild(li);
+                })
+        })
+        .catch((err) => {
+            stopNameDiv.textContent = 'Error'
+        })
 }
