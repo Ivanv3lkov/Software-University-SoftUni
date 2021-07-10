c540a1954b86aba4bf767d203c290a3b2ed681d3
ivanv3lkov <ivanv3lkov@gmail.com>
2021-07-10 09:47:45 +0300

    Asynchronous programming exercise

--- JS Applications/JS Applications June 2021/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/06.Blog/app.js
+++ JS Applications/JS Applications June 2021/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/06.Blog/app.js
@@ -1,5 +1,54 @@
 function attachEvents() {
-    console.log('TODO...');
+    let baseURL = 'http://localhost:3030/jsonstore/blog';
+    let buttonLoadPosts = document.getElementById('btnLoadPosts');
+    let buttonViewPosts = document.getElementById('btnViewPost');
+    let select = document.getElementById('posts');
+    let postTitleEl = document.getElementById('post-title');
+    let postCommentsEl = document.getElementById('post-comments');
+    let postId;
+
+    buttonLoadPosts.addEventListener('click', () => {
+        select.innerHTML = '';
+        fetch(`${baseURL}/posts`)
+            .then(res => res.json())
+            .then(data => {
+                Object.entries(data)
+                    .forEach(([key, valueObj]) => {
+                        let option = document.createElement('option');
+                        postId = valueObj.id;
+                        option.value = key;
+                        option.textContent = valueObj.title.toUpperCase();
+                        select.appendChild(option);
+                    })
+            })
+    })
+
+    buttonViewPosts.addEventListener('click', () => {
+        fetch(`${baseURL}/posts/${select.value}`)
+            .then(res => res.json())
+            .then(({ body, id, title }) => {
+                let postBody = document.getElementsByTagName('body')[0].children[5];
+                postTitleEl.textContent = title;
+                let p = document.createElement('p');
+                p.setAttribute('id', 'post-body');
+                p.textContent = body;
+                postBody.replaceWith(p);
+
+                fetch(`${baseURL}/comments`)
+                    .then(res => res.json())
+                    .then((data) => {
+                        postCommentsEl.innerHTML = '';
+                        Object.entries(data)
+                            .forEach(([commentId, value]) => {
+                                if (value.postId == id) {
+                                    let li = document.createElement('li');
+                                    li.textContent = value.text;
+                                    postCommentsEl.appendChild(li);
+                                }
+                            })
+                    })
+            })
+    })
 }
 
 attachEvents();
