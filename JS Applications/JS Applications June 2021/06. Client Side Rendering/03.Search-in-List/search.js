7ee2d4a8dabe410b197f082ea6c005633b8d591b
ivanv3lkov <ivanv3lkov@gmail.com>
2021-07-25 11:33:07 +0300

    exercise

--- JS Applications/JS Applications June 2021/06. Client Side Rendering/03.Search-in-List/search.js
+++ JS Applications/JS Applications June 2021/06. Client Side Rendering/03.Search-in-List/search.js
@@ -1,3 +1,25 @@
-function search() {
-   // TODO
+import { render } from './../node_modules/lit-html/lit-html.js';
+import { towns } from './towns.js';
+import { townsTemplate } from './templates/townsTemplates.js';
+
+let townsDiv = document.getElementById('towns');
+let baseTowns = towns.map(t => ({name: t}));
+
+render(townsTemplate(baseTowns), townsDiv);
+
+let resultDiv = document.getElementById('result');
+
+let searchButton = document.getElementById('search-btn');
+searchButton.addEventListener('click', searchHandler);
+
+function searchHandler() {
+   let searchInput = document.getElementById('searchText');
+   let searchText = searchInput.value.toLowerCase();
+
+   let allTowns = towns.map(t => ({ name: t }));
+   let matchedTowns = allTowns.filter(t => t.name.toLowerCase().includes(searchText));
+   matchedTowns.forEach(t => t.class = 'active');
+
+   render(townsTemplate(allTowns), townsDiv);
+   resultDiv.textContent = `${matchedTowns.length} matches found`;
 }
---
+++ JS Applications/JS Applications June 2021/06. Client Side Rendering/03.Search-in-List/templates/townsTemplates.js
@@ -0,0 +1,9 @@
+import { html } from './../../node_modules/lit-html/lit-html.js';
+import {ifDefined} from './../../node_modules/lit-html/directives/if-defined.js';
+
+export let townTemplate = (town) => html`<li class=${ifDefined(town.class)}>${town.name}</li>`;
+
+export let townsTemplate = (towns) => html`
+<ul>
+    ${towns.map(t => townTemplate(t))}
+</ul>`;
