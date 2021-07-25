9b7edb756dbde72efa13c0625952d8db40ac8205
ivanv3lkov <ivanv3lkov@gmail.com>
2021-07-25 12:31:05 +0300

    exercise

--- JS Applications/JS Applications June 2021/06. Client Side Rendering/04.Fill-Dropdown/dropdown.js
+++ JS Applications/JS Applications June 2021/06. Client Side Rendering/04.Fill-Dropdown/dropdown.js
@@ -2,6 +2,7 @@ import { render } from './../node_modules/lit-html/lit-html.js';
 import { allOptionsTemplate } from './templates/optionTemplates.js';
 
 let menuSelect = document.getElementById('menu');
+let inputField = document.getElementById('itemText');
 
 let addOptionForm = document.getElementById('add-option-form');
 addOptionForm.addEventListener('submit', addItem);
@@ -43,7 +44,15 @@ async function addItem(e) {
 
     if (createResponse.ok) {
         let createdOption = await createResponse.json();
-        options.push(createdOption);
+        let isOptionExists = options.find(option => option.text === createdOption.text);
+        if (isOptionExists) {
+            inputField.classList.add('error');
+            alert('This option already exists in the section! Please enter a new option!');
+        } else {
+            inputField.classList.remove('error');
+            inputField.value = '';
+            options.push(createdOption);
+        }
         render(allOptionsTemplate(options), menuSelect);
     }
 }
