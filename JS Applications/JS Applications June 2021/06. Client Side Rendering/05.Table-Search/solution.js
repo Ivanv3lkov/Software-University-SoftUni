fe30367725524808221ea2159a124327d0517a6e
ivanv3lkov <ivanv3lkov@gmail.com>
2021-07-25 12:53:28 +0300

    exercise

--- JS Applications/JS Applications June 2021/06. Client Side Rendering/05.Table-Search/solution.js
+++ JS Applications/JS Applications June 2021/06. Client Side Rendering/05.Table-Search/solution.js
@@ -1,8 +1,39 @@
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+import { render } from './../node_modules/lit-html/lit-html.js';
+import { allStudentsTemplate } from './template/studentTemplates.js';
 
-   function onClick() {
-      //   TODO:
+document.getElementById('searchBtn').addEventListener('click', onClickHandler);
+let tableTbody = document.querySelector('.container tbody');
 
-   }
+let students = [];
+
+loadStudents();
+
+async function loadStudents() {
+   let studentsResponse = await fetch('http://localhost:3030/jsonstore/advanced/table');
+   let studentsObj = await studentsResponse.json();
+   
+   students = Object.values(studentsObj).map(student => ({
+      name: `${student.firstName} ${student.lastName}`,
+      course: student.course,
+      email: student.email
+   }));
+
+   render(allStudentsTemplate(students), tableTbody);
+}
+
+function onClickHandler() {
+   let searchInput = document.getElementById('searchField');
+   let searchText = searchInput.value.toLowerCase();
+
+   if (!searchText) {return;}
+
+   let allStudents = students.map(student => Object.assign({}, student));
+   let matchedStudents = allStudents
+      .filter(student => Object.values(student).some(val => {
+         return val.toLowerCase().includes(searchText)
+      }));
+   matchedStudents.forEach(s => s.class = 'select');
+
+   searchInput.value = '';
+   render(allStudentsTemplate(allStudents), tableTbody);
 }
--- JS Applications/JS Applications June 2021/06. Client Side Rendering/05.Table-Search/template/studentTemplates.js
+++ JS Applications/JS Applications June 2021/06. Client Side Rendering/05.Table-Search/template/studentTemplates.js
@@ -1,2 +1,11 @@
 import { html } from './../../node_modules/lit-html/lit-html.js';
+import {ifDefined} from './../../node_modules/lit-html/directives/if-defined.js';
 
+export let studentTemplate = student => html`
+    <tr class=${ifDefined(student.class)}>
+        <td>${student.name}</td>
+        <td>${student.email}</td>
+        <td>${student.course}</td>
+    </tr>`;
+
+export let allStudentsTemplate = students => html`${students.map(student => studentTemplate(student))}`;
