0472c2662f2ac0a6ec3e0395efee56d0997f7642
Ivan Velkov <ivanv3lkov@gmail.com>
2022-06-26 18:39:22 +0300

    Align content within the middlewares

--- JS Back-End/exam-prep-artGallery-app/middlewares/authMiddleware.js
+++ JS Back-End/exam-prep-artGallery-app/middlewares/authMiddleware.js
@@ -1,19 +1,15 @@
 const jwt = require('jsonwebtoken');
 
-const {COOKIE_SESSION_NAME} = require('../constants');
-const {SECRET} = require('../config/env');
-
-
-
+const { COOKIE_SESSION_NAME } = require('../constants');
+const { SECRET } = require('../config/env');
 
 exports.auth = (req, res, next) => {
     const token = req.cookies[COOKIE_SESSION_NAME];
 
-    if(token){
+    if (token) {
         jwt.verify(token, SECRET, ((err, decodedToken) => {
             if (err) {
                 res.clearCookie(COOKIE_SESSION_NAME);
-                // return next(err);
                 return res.redirect('/auth/login')
             }
             req.user = decodedToken;
@@ -25,17 +21,16 @@ exports.auth = (req, res, next) => {
     }
 };
 
-
 exports.isAuth = (req, res, next) => {
     if (!req.user) {
         return res.redirect('/auth/login');
-    } 
+    }
     next();
 };
 
 exports.isGuest = (req, res, next) => {
     if (req.user) {
         return res.redirect('/');
-    } 
+    }
     next();
 };
